2ab7821a0dddfd0698bef1b7e9c45595a7f8e8e0
iamaryanyadavv <aryanyadav1601@gmail.com>
2024-03-30 15:43:45 +0530

    added easter egg

--- src/components/comp2/comp2.js
+++ src/components/comp2/comp2.js
@@ -3,27 +3,42 @@ import './comp2.css';
 import { MdArrowOutward } from "react-icons/md";
 
 function Comp2() {
-  const handleButtonClick = () => {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-  };
+    const handleButtonClick = () => {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+    };
 
-  return (
-    <div className="component2">
-        <div className='component2content'>
-            <h4 data-aos="fade-down">Lorem Ipsum dolor</h4>
-            <p style={{marginBottom: '60px', fontSize: '20px'}}>Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph
-               Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph
-               Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph Lorem Ipsum dolor paragraph
-               Lorem Ipsum</p>
-            <div className="right-aligned-content">
-                <p className="right-aligned-text">Lorem ipsum sit amet</p>
-                <button className="rounded-button" onClick={handleButtonClick}>
-                    <span style={{alignItems: 'center'}}>Lorem Ipsum <MdArrowOutward size={24} color="black" className="menu-navbar-bottomicon" /></span>
-                </button>
+    return (
+        <div className="component2">
+            <div className='component2content'>
+                <h4 data-aos="fade-down" style={{fontSize: '20px', maringBottom: '24px'}}>Lorem Ipsum</h4>
+                <p style={{ marginBottom: '60px', fontSize: '20px', maxWidth: '40px' }}>
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⠿⠟⠛⠻⣿⠆⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣆⣀⣀⠀⣿⠂⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠻⣿⣿⣿⠅⠛⠋⠈⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢼⣿⣿⣿⣃⠠⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣟⡿⠃⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⣛⣫⡄⠀⢸⣦⣀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡆⠸⣿⣿⣿⡷⠂⠨⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⡇⢀⣿⡿⠋⠁⢀⡶⠪⣉⢸⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⣷⣿⣿⣷⣦⡙⣿⣿⣿⣿⣿⡏⠀⠀
+                    ⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+                    ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀
+                    ⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀
+                    ⠀⠀⠀⢹⣿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                </p>
+                <div className="right-aligned-content">
+                    <p className="right-aligned-text">Lorem ipsum sit amet</p>
+                    <button className="rounded-button" onClick={handleButtonClick}>
+                        <span style={{ alignItems: 'center', display: 'flex', fontSize: '20px', fontWeight: '500' }}>What is this?</span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Comp2;
