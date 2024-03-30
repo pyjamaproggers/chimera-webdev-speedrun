b3f8ce00fdea341f243003d0d49ee0b1a91ee381
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-03-30 13:26:03 +0530

    Temp font

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Grey from './assets/Grey.jpeg'
 import Comp1 from './components/comp1/comp1';
 import Comp2 from './components/comp2/comp2';
 import Comp3 from './components/comp3/comp3';
+import Comp4 from './components/comp4/comp4';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
                     <Comp1/>
                     <Comp2/>
                     <Comp3/>
+                    <Comp4/>
                 </div>
             </div>
         </>
--- src/components/comp4/comp4.js
+++ src/components/comp4/comp4.js
@@ -4,7 +4,14 @@ import './comp4.css';
 function Comp4() {
   return (
     <div className="component4">
-    
+      <p>Lorem ipsum dolor sit</p>
+      <h1 className="extremely-large">LoremIpsum</h1>
+      <p className="with-line">Lorem</p>
+      <div className="with-arrows">
+        <span>Lorem</span> <span className="arrow">{'->'}</span> 
+        <span>Ipsum</span> <span className="arrow">{'->'}</span> 
+        <span>Dolor</span> <span className="arrow">{'->'}</span> 
+      </div>
     </div>
   );
 }
